37e49e2357089a7c52861d3425932038a59e0e4f
Pomarancz789 <granat-02krawcy@icloud.com>
2022-01-12 10:30:40 +0100

    Initial Commit

---
+++ srotowanie-12-stycznina/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  srotowanie-12-stycznina
+//
+//  Created by Jakub Wtorkiewicz on 12/01/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
